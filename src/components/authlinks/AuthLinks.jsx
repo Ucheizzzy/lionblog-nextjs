120452c45f098c133dfc6af7fbf656171e873eda
Mr.Lion <uchechukwuobiakor@gmail.com>
2023-12-21 21:12:06 +0100

    working on the featured section

--- src/components/authlinks/AuthLinks.jsx
+++ src/components/authlinks/AuthLinks.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react'
 
 export default function AuthLinks() {
   const [open, setOpen] = useState(false)
+
   const status = 'notAuthenticated'
   return (
     <>
@@ -19,6 +20,28 @@ export default function AuthLinks() {
           </Link>
           <span className={styles.link}>Logout</span>
         </>
+      )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu} onClick={() => setOpen(!open)}>
+          <Link href='/'>Home</Link>
+          <Link href='/contact'>Contact</Link>
+          <Link href='/about'>About</Link>
+          {status === 'notAuthenticated' ? (
+            <Link href='/login'>Login</Link>
+          ) : (
+            <>
+              <Link href='/write'>Write</Link>
+              <Link href='/logout' className={styles.link}>
+                Logout
+              </Link>
+            </>
+          )}
+        </div>
       )}
     </>
   )
